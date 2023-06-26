ba9a64dccd6a3d084b3ead00e620b62a320fe7a7
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-06-26 11:37:33 +0200

    add handling for the fields and letters position status
    - add styles depends on field status

--- src/components/word/Field.jsx
+++ src/components/word/Field.jsx
@@ -4,8 +4,13 @@ const Field = props => {
 
     const {value, status} = props
 
+    let statusStyle
+
+    if(status === "perfect") statusStyle = style.perfect
+    if(status === "good") statusStyle = style.good
+
     return (
-        <div className={style.field}>{value}</div>
+        <div className={`${style.field} ${statusStyle}`}>{value}</div>
     )
 }
 
--- src/components/word/Word.jsx
+++ src/components/word/Word.jsx
@@ -1,11 +1,12 @@
 import {useState, useEffect} from "react";
 import Field from "./Field.jsx";
 import style from "./Word.module.css"
+import field from "./Field.jsx";
 
 const Word = ({letter}) => {
 
     const [letterIndex, setLetterIndex] = useState(0)
-    const DUMMY_TEXT = "tekst"
+    const DUMMY_TEXT = "tekst".toUpperCase()
     let activeRow
 
 
@@ -13,32 +14,50 @@ const Word = ({letter}) => {
         {
             id: 0,
             isLocked: false,
-            fields: ["", "", "", "", ""]
+            fields: [{value: "", status: "unchecked"}, {value: "", status: "unchecked"}, {
+                value: "",
+                status: "unchecked"
+            }, {value: "", status: "unchecked"}, {value: "", status: "unchecked"}]
         },
         {
             id: 1,
             isLocked: false,
-            fields: ["", "", "", "", ""]
+            fields: [{value: "", status: "unchecked"}, {value: "", status: "unchecked"}, {
+                value: "",
+                status: "unchecked"
+            }, {value: "", status: "unchecked"}, {value: "", status: "unchecked"}]
         },
         {
             id: 2,
             isLocked: false,
-            fields: ["", "", "", "", ""]
+            fields: [{value: "", status: "unchecked"}, {value: "", status: "unchecked"}, {
+                value: "",
+                status: "unchecked"
+            }, {value: "", status: "unchecked"}, {value: "", status: "unchecked"}]
         },
         {
             id: 3,
             isLocked: false,
-            fields: ["", "", "", "", ""]
+            fields: [{value: "", status: "unchecked"}, {value: "", status: "unchecked"}, {
+                value: "",
+                status: "unchecked"
+            }, {value: "", status: "unchecked"}, {value: "", status: "unchecked"}]
         },
         {
             id: 4,
             isLocked: false,
-            fields: ["", "", "", "", ""]
+            fields: [{value: "", status: "unchecked"}, {value: "", status: "unchecked"}, {
+                value: "",
+                status: "unchecked"
+            }, {value: "", status: "unchecked"}, {value: "", status: "unchecked"}]
         },
         {
             id: 5,
             isLocked: false,
-            fields: ["", "", "", "", ""]
+            fields: [{value: "", status: "unchecked"}, {value: "", status: "unchecked"}, {
+                value: "",
+                status: "unchecked"
+            }, {value: "", status: "unchecked"}, {value: "", status: "unchecked"}]
         }
     ])
 
@@ -56,12 +75,30 @@ const Word = ({letter}) => {
             case "":
                 break
             case "ENTER":
+                if (letterIndex === activeRow.fields.length - 1) {
+                    activeRow.isLocked = true
+                    let userWord = ""
+                    activeRow.fields.forEach((field, index) => {
+                        userWord += field.value
+                        if(field.value === DUMMY_TEXT[index]) {
+                            field.status = "perfect"
+                        } else if(DUMMY_TEXT.includes(field.value)) {
+                            field.status = "good"
+                        } else {
+                            field.status = "wrong"
+                        }
 
+                        if(userWord === DUMMY_TEXT) winGame()
+                    })
+                    setLetterIndex(0)
+                } else {
+                    console.log("You must provide all fields in the row")
+                }
                 break
             case "<-":
                 break
             default:
-                activeRow.fields[letterIndex] = letter
+                activeRow.fields[letterIndex].value = letter
                 if (letterIndex < activeRow.fields.length - 1) {
                     setLetterIndex(prevIndex => prevIndex + 1)
                 }
@@ -79,12 +116,16 @@ const Word = ({letter}) => {
 
     }, [letter])
 
+    const winGame = () => {
+        console.log("You won the game")
+    }
+
     return (
         <div className={style.container}>
             {structure.map((row, index) => {
                 return <div key={index} className={style.row}>
                     {row.fields.map((field, index) => {
-                        return <Field key={index} value={field}/>
+                        return <Field key={index} value={field.value} status={field.status}/>
                     })}
                 </div>
             })}
